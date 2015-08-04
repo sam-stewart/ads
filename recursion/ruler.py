fc8e5cc1148a85c80e8d1b842b0a73a55ec2835c
Samuel Stewart <samuelcstewart@icloud.com>
2015-08-04 23:44:36 +1200

    Added my walk function

---
+++ recursion/ruler.py
@@ -0,0 +1,34 @@
+def draw_line(tick_length, tick_label = ''):
+    line = '-' * tick_length
+    if tick_label:
+        line += ' ' + tick_label
+    print(line)
+
+def draw_interval(center_length):
+    if center_length > 0:
+        draw_interval(center_length -1)
+        draw_line(center_length)
+        draw_interval(center_length -1)
+
+def draw_ruler(num_inches, major_length):
+    draw_line(major_length, '0')
+    for j in range(1, 1 + num_inches):
+        draw_interval(major_length -1)
+        draw_line(major_length, str(j))
+
+
+# Binary search
+
+def binary_search(data, target, low, high):
+    if low > high:
+        return False
+    else:
+        mid = (low + high) / 2
+        if target == data[mid]:
+            return True
+        elif target < data[mid]:
+            # recur on left of middle
+            return binary_search(data, target, low, mid -1)
+        else:
+            # recur right of middle
+            return binary_search(data, target, mid + 1, high)
