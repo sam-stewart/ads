dd44bafb4c274d26cd7e3d1ccfbf34d50129f398
Samuel Stewart <samuelcstewart@icloud.com>
2015-10-17 00:15:13 +1300

    swap out list comprehensions for set comprehensions

--- spellchecker/spellchecker.py
+++ spellchecker/spellchecker.py
@@ -20,35 +20,35 @@ class SpellChecker(object):
         clist = self.cache.get(s)
         if clist is not None:
             return clist
-        possibles = set(self.transpose(s) + self.extrachar(s) +
-                        self. missingchar(s) + self.delchar(s) + self.replace(s))
+        possibles = self.transpose(s) | self.extrachar(s) | \
+                        self.missingchar(s) | self.delchar(s) | self.replace(s)
         clist = list(self.dictionary.intersection(possibles))
         self.cache[s] = clist
         return clist
 
     def transpose(self, s):
-        """ Return a list of all adjacent character transpositions of s """
-        return [s[:i] + s[i+1] + s[i] + s[i+2:] for i in range(len(s) - 1)]
+        """ Return a set of all adjacent character transpositions of s """
+        return {s[:i] + s[i+1] + s[i] + s[i+2:] for i in range(len(s) - 1)}
 
     def extrachar(self, s):
-        """ Return a list of all possible strings with a character injected
+        """ Return a set of all possible strings with a character injected
         between two adjacent characters """
-        return [s[:i+1] + c + s[i+1:] for i in range(len(s) - 1)
-                for c in string.ascii_lowercase]
+        return {s[:i+1] + c + s[i+1:] for i in range(len(s) - 1)
+                for c in string.ascii_lowercase}
 
     def missingchar(self, s):
-        """ Return a list of all possible strings assuming user missed a single
+        """ Return a set of all possible strings assuming user missed a single
         character """
-        return [s[:i] + c + s[i:] for i in range(len(s) + 1)
-                for c in string.ascii_lowercase]
+        return {s[:i] + c + s[i:] for i in range(len(s) + 1)
+                for c in string.ascii_lowercase}
 
     def delchar(self, s):
-        """ Return a list of possible strings with a single character deleted
+        """ Return a set of possible strings with a single character deleted
         from the original word """
-        return [s[:i] + s[i+1:] for i in range(len(s))]
+        return {s[:i] + s[i+1:] for i in range(len(s))}
 
     def replace(self, s):
-        """ Return a list of all possible strings resulting from replacing a
+        """ Return a set of all possible strings resulting from replacing a
         single character with another one """
-        return [s[:i] + c + s[i+1:] for i in range(len(s))
-                for c in string.ascii_lowercase]
+        return {s[:i] + c + s[i+1:] for i in range(len(s))
+                for c in string.ascii_lowercase}
