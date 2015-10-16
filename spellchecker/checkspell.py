0de54375249b24c0c02df335b2461b97e9937338
Samuel Stewart <samuelcstewart@icloud.com>
2015-10-16 14:51:24 +1300

    Refactor spellchecker into a class. Add command line argument parsing

--- spellchecker/checkspell.py
+++ spellchecker/checkspell.py
@@ -1,24 +1,39 @@
-#!/usr/bin/python
-
 import sys
-import spellchecker
+import argparse
+from spellchecker import SpellChecker
 import string
 
 
-def getline():
+def getline(f):
     """ return a tuple containing a list of words pertaining to a line from a
     file and the line number """
     count = 1
-    for line in open(sys.argv[1]):
+    for line in f:
         yield line.lower().split(), count
         count += 1
 
-if __name__ == "__main__":
-    for words, l in getline():
+
+def main():
+    parser = argparse.ArgumentParser(description="Spellcheck a given text \
+                                     file against a given dictionary file")
+    parser.add_argument('-f', '--file', type=file, dest='file',
+                        help='file on which to run a spellcheck')
+    parser.add_argument('-d', '--dictionary', type=file, dest='dictionary',
+                        help='file containing correctly spelled words, each \
+                        defined on a new line')
+    args = parser.parse_args()
+
+    checker = SpellChecker(args.dictionary)
+
+    for words, l in getline(args.file):
         for w in words:
             w = w.translate(None, string.punctuation)
-            checked = spellchecker.check(w)
+            checked = checker.check(w)
             if not checked:
                 print str(l) + "\t" + w + "\t" + "No corrections"
             elif checked[0] != w:
                 print str(l) + "\t" + w + "\t" + str(checked)
+
+
+if __name__ == "__main__":
+    main()
--- spellchecker/spellchecker.py
+++ spellchecker/spellchecker.py
@@ -1,40 +1,46 @@
 import string
-from collections import OrderedDict
 
-dictionary = set(line.strip() for line in open('words'))
-cachedict = OrderedDict()
 
-def check(s):
-    if s in dictionary:
-        return [s]
-    possibles = set(transpose(s) + extrachar(s) + missingchar(s) +
-                    delchar(s) + replace(s))
-    cachedict[s] = {'cwords': possibles, 'count': 0}
-    return list(dictionary.intersection(possibles))
+class SpellChecker(object):
+    """ Python spell checker implemented as described in problem 10.55 in
+    Algorithms and Structures with python. """
 
-def transpose(s):
-    """ Return a list of all adjacent character transpositions of s """
-    return [s[:i] + s[i+1] + s[i] + s[i+2:] for i in range(len(s) - 1)]
+    def __init__(self, dictfile):
+        """ Accept a file reference and parse it into a set. Dictionary file
+        should define each word on a new line """
+        self.dictionary = set(line.strip().lower() for line in dictfile)
 
-def extrachar(s):
-    """ Return a list of all possible strings with a character injected
-    between two adjacent characters """
-    return [s[:i+1] + c + s[i+1:] for i in range(len(s) - 1)
-            for c in string.ascii_lowercase]
+    def check(self, s):
+        if s in self.dictionary:
+            return [s]
+        possibles = set(self.transpose(s) + self.extrachar(s) +
+                        self. missingchar(s) + self.delchar(s) + self.replace(s))
+        #cachedict[s] = {'cwords': possibles, 'count': 0}
+        return list(self.dictionary.intersection(possibles))
 
-def missingchar(s):
-    """ Return a list of all possible strings assuming user missed a single
-    character """
-    return [s[:i] + c + s[i:] for i in range(len(s) + 1)
-            for c in string.ascii_lowercase]
+    def transpose(self, s):
+        """ Return a list of all adjacent character transpositions of s """
+        return [s[:i] + s[i+1] + s[i] + s[i+2:] for i in range(len(s) - 1)]
 
-def delchar(s):
-    """ Return a list of possible strings with a single character deleted from
-    the original word """
-    return [s[:i] + s[i+1:] for i in range(len(s))]
+    def extrachar(self, s):
+        """ Return a list of all possible strings with a character injected
+        between two adjacent characters """
+        return [s[:i+1] + c + s[i+1:] for i in range(len(s) - 1)
+                for c in string.ascii_lowercase]
 
-def replace(s):
-    """ Return a list of all possible strings resulting from replacing a
-    single character with another one """
-    return [s[:i] + c + s[i+1:] for i in range(len(s))
-            for c in string.ascii_lowercase]
+    def missingchar(self, s):
+        """ Return a list of all possible strings assuming user missed a single
+        character """
+        return [s[:i] + c + s[i:] for i in range(len(s) + 1)
+                for c in string.ascii_lowercase]
+
+    def delchar(self, s):
+        """ Return a list of possible strings with a single character deleted
+        from the original word """
+        return [s[:i] + s[i+1:] for i in range(len(s))]
+
+    def replace(self, s):
+        """ Return a list of all possible strings resulting from replacing a
+        single character with another one """
+        return [s[:i] + c + s[i+1:] for i in range(len(s))
+                for c in string.ascii_lowercase]
