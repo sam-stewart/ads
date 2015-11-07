45f08cabefa5abd8aab9efa111d231dac5a894c7
Samuel Stewart <samuelcstewart@icloud.com>
2015-11-08 02:21:09 +1300

    Slight fizzbuzz refactor

--- assignment2/fizzbuzz/fizzbuzz.py
+++ assignment2/fizzbuzz/fizzbuzz.py
@@ -3,13 +3,14 @@
 
 def fizzbuzz(x):
     """ Return Fizz if x is multiple of 3, Buzz if multiple of 5, FizzBuzz if
-    multiple of three and five. Maximum three comparisons"""
-    if x % 3 == 0:
-        return 'Fizz'
+    multiple of three and five. Maximum two comparisons. There are less
+    multiples of 5 than 3, so testing for three nested in 5 makes sense. """
     if x % 5 == 0:
         if x % 3 == 0:
             return 'FizzBuzz'
         return 'Buzz'
+    if x % 3 == 0:
+        return 'Fizz'
     return str(x)
 
 if __name__ == '__main__':
