e04522441ddec59d7e376b444ee9e9019772b71e
Samuel Stewart <samuelcstewart@icloud.com>
2015-11-18 00:02:42 +1300

    add left/right variables to correct evaluation order when popping off the FIFO stack.

--- assignment2/postfix/postfixcalc.py
+++ assignment2/postfix/postfixcalc.py
@@ -5,6 +5,9 @@ import operator
 
 
 def calc(chars):
+    """ Take a postfix expression string, eg.) '3 4 + 7 *', evaluate and
+    return the result """
+    chars = chars.split()
     ops = {'+': operator.add,
         '-': operator.sub,
         '*': operator.mul,
@@ -14,7 +17,9 @@ def calc(chars):
 
     for x in chars:
         if len(stack) == 2:
-            stack.push(ops[x](stack.pop(), stack.pop()))
+            right = stack.pop()
+            left = stack.pop()
+            stack.push(ops[x](left, right))
         else:
             stack.push(int(x))
     return stack.pop()
@@ -23,7 +28,7 @@ def main():
     if len(sys.argv) != 2:
         print "Wrong number of arguments"
         sys.exit(1)
-    print calc(sys.argv[1].split())
+    print calc(sys.argv[1])
 
 if __name__ == '__main__':
     main()
