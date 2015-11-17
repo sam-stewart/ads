1b27d868c9fe6c0a8cff0c1bb528091d44986c9b
Samuel Stewart <samuelcstewart@icloud.com>
2015-11-17 23:45:16 +1300

    Refactor postfix calc

--- assignment2/postfix/postfixcalc.py
+++ assignment2/postfix/postfixcalc.py
@@ -3,22 +3,27 @@ from stack import Stack
 import sys
 import operator
 
-if len(sys.argv) != 2:
-    print "Wrong number of arguments"
-    sys.exit(1)
 
-ops = {'+': operator.add,
-       '-': operator.sub,
-       '*': operator.mul,
-       '/': operator.div}
+def calc(chars):
+    ops = {'+': operator.add,
+        '-': operator.sub,
+        '*': operator.mul,
+        '/': operator.div}
 
-chars = sys.argv[1].split()
-stack = Stack()
+    stack = Stack()
 
-for x in chars:
-    if len(stack) == 2:
-        stack.push(ops[x](stack.pop(), stack.pop()))
-    else:
-        stack.push(int(x))
+    for x in chars:
+        if len(stack) == 2:
+            stack.push(ops[x](stack.pop(), stack.pop()))
+        else:
+            stack.push(int(x))
+    return stack.pop()
 
-print stack.pop()
+def main():
+    if len(sys.argv) != 2:
+        print "Wrong number of arguments"
+        sys.exit(1)
+    print calc(sys.argv[1].split())
+
+if __name__ == '__main__':
+    main()
